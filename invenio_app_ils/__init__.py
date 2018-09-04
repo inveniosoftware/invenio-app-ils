62b493e391aaeb40f1ff880a928ac3ef7b44e5e9
Zacharias Zacharodimos <zacharias.zacharodimos@cern.ch>
2018-09-04 12:24:11 +0200

    global:initial cookiecutter structure

---
+++ invenio_app_ils/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2018 CERN.
+#
+# invenio-app-ils is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""invenio-app-ils."""
+
+from __future__ import absolute_import, print_function
+
+from .version import __version__
+
+__all__ = ('__version__', )
---
+++ invenio_app_ils/version.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2018 CERN.
+#
+# invenio-app-ils is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Version information for invenio-app-ils.
+
+This file is imported by ``invenio_app_ils.__init__``,
+and parsed by ``setup.py``.
+"""
+
+from __future__ import absolute_import, print_function
+
+__version__ = '1.0.0'
