e69142c52c4406bb4652bbae80ba86b49a225280
Chris Topaloudis <topless@gmail.com>
2018-10-19 16:46:50 +0200

    backoffice: introduced main page
    * Main routed page for backoffice
    * Styled NotFound and redirect to main

--- invenio_app_ils/ui/backoffice/src/App.js
+++ invenio_app_ils/ui/backoffice/src/App.js
@@ -3,9 +3,10 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { Navbar } from './common/components/Navbar';
+import { Backoffice } from './pages/Backoffice';
 import { LoanDetails } from './pages/LoanDetails';
 import { ItemDetails } from './pages/ItemDetails';
-import NotFound from './common/components/NotFound';
+import { NotFound } from './common/components/NotFound';
 
 import './App.scss';
 
@@ -17,6 +18,7 @@ export default class App extends Component {
           <Navbar />
           <div className="app-content">
             <Switch>
+              <Route path="/backoffice" component={Backoffice} />
               <Route path="/backoffice/loans/:recid" component={LoanDetails} />
               <Route path="/backoffice/items/:recid" component={ItemDetails} />
               <Route component={NotFound} />
