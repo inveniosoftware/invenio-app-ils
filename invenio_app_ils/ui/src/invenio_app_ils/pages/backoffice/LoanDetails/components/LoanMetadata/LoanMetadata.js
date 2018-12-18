7962cdcde1ac25859eb8557f7c130c8bfba2c7a3
Christos Topaloudis <topless@gmail.com>
2018-12-18 02:07:01 +0100

    Loan details with layout same as Item

--- invenio_app_ils/ui/src/invenio_app_ils/pages/backoffice/LoanDetails/components/LoanMetadata/LoanMetadata.js
+++ invenio_app_ils/ui/src/invenio_app_ils/pages/backoffice/LoanDetails/components/LoanMetadata/LoanMetadata.js
@@ -1,27 +1,65 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Grid, Segment, Form } from 'semantic-ui-react';
+import { Grid, Segment, Header, Table } from 'semantic-ui-react';
 
 export default class LoanMetadata extends Component {
-  renderLoanMetadata(loanDetails) {
-    return Object.keys(loanDetails).map(key => {
-      return (
-        <Form.Field key={key} data-test="loan-field">
-          <label className="field-name">{key}</label>
-          <p>{loanDetails[key]}</p>
-        </Form.Field>
-      );
-    });
-  }
-
   render() {
-    const loanDetails = this.props.loanDetails.metadata;
+    const data = this.props.loanDetails.metadata;
     return (
       <Segment>
-        <Grid>
+        <Grid padded columns={2}>
+          <Grid.Column width={16}>
+            <Header as="h1">Loan - {data.loan_pid}</Header>
+          </Grid.Column>
+
           <Grid.Column>
-            <h1>Loan - {loanDetails.loan_pid}</h1>
-            <Form>{this.renderLoanMetadata(loanDetails)}</Form>
+            <Table basic="very" definition>
+              <Table.Body>
+                <Table.Row>
+                  <Table.Cell width={3}>Item pid</Table.Cell>
+                  <Table.Cell width={12}>{data.item_pid}</Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell width={3}>Patron pid</Table.Cell>
+                  <Table.Cell width={12}>{data.patron_pid}</Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell width={3}>Pickup location pid</Table.Cell>
+                  <Table.Cell width={12}>{data.pickup_location_pid}</Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell width={3}>Transaction Location pid</Table.Cell>
+                  <Table.Cell width={12}>
+                    {data.transaction_location_pid}
+                  </Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell width={3}>Transaction User pid</Table.Cell>
+                  <Table.Cell width={12}>
+                    {data.transaction_user_pid}
+                  </Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell width={3}>State</Table.Cell>
+                  <Table.Cell width={12}>{data.state}</Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            </Table>
+          </Grid.Column>
+
+          <Grid.Column>
+            <Table basic="very" definition>
+              <Table.Body>
+                <Table.Row>
+                  <Table.Cell width={3}>Transaction date</Table.Cell>
+                  <Table.Cell width={12}>{data.transaction_date}</Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell width={3}>Expire Date</Table.Cell>
+                  <Table.Cell width={12}>{data.request_expire_date}</Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            </Table>
           </Grid.Column>
         </Grid>
       </Segment>
