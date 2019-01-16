5968ea00dd90efae8caef11f2fc12bbdab225522
Karolina Przerwa <karolina.zdzislawa.przerwa@cern.ch>
2019-01-16 11:31:12 +0100

    ui: usage of ResultsTable in components
    * refactored displayed layer of components
    
    * tests

--- invenio_app_ils/ui/src/invenio_app_ils/common/components/ResultsTable/ResultsTable.js
+++ invenio_app_ils/ui/src/invenio_app_ils/common/components/ResultsTable/ResultsTable.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Segment, Message, Header, Table } from 'semantic-ui-react';
-import TableHeader from './TableHeader';
-import TableBody from './TableBody';
-import TableFooter from './TableFooter';
+import ResultsTableHeader from './ResultsTableHeader';
+import ResultsTableBody from './ResultsTableBody';
+import ResultsTableFooter from './ResultsTableFooter';
 
 export class ResultsTable extends Component {
   constructor(props) {
@@ -15,27 +15,26 @@ export class ResultsTable extends Component {
   _renderTable = () => {
     const { rows, showMaxRows } = this.props;
     const columns = rows ? Object.keys(rows[0]) : [];
-    return rows.length ? (
+    return (
       <Table striped singleLine selectable>
-        <TableHeader columns={columns} />
-        <TableBody
+        <ResultsTableHeader columns={columns} />
+        <ResultsTableBody
           columns={columns}
           rows={rows.slice(0, showMaxRows)}
           detailsClickHandler={this.detailsClickHandler}
         />
-        <TableFooter
+        <ResultsTableFooter
           allRowsNumber={rows.length}
           showMaxRows={this.props.showMaxRows}
           showAllClickHandler={this.showAllClickHandler}
           columnsNumber={columns.length}
         />
       </Table>
-    ) : null;
+    );
   };
 
   _renderResultsOrEmpty() {
     const { rows, name } = this.props;
-
     return rows.length ? (
       this._renderTable(rows)
     ) : (
