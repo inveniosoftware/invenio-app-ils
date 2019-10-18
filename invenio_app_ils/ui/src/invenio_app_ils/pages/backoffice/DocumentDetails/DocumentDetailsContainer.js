408abe592132b57dabf16c88ba197411417713ad
Chris Topaloudis <topless@gmail.com>
2019-10-18 16:42:41 +0200

    DocumentDetails link when navigate to another DocumentDetails

--- invenio_app_ils/ui/src/invenio_app_ils/pages/backoffice/DocumentDetails/DocumentDetailsContainer.js
+++ invenio_app_ils/ui/src/invenio_app_ils/pages/backoffice/DocumentDetails/DocumentDetailsContainer.js
@@ -2,26 +2,16 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Container } from 'semantic-ui-react';
 import { DocumentDetails } from './components';
-import history from '../../../history';
 
 export default class DocumentDetailsContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.deleteDocument = this.props.deleteDocument;
-    this.fetchDocumentDetails = this.props.fetchDocumentDetails;
-  }
-
   componentDidMount() {
-    this.unlisten = history.listen(loc => {
-      if (loc.state && loc.state.pid && loc.state.type === 'Document') {
-        this.fetchDocumentDetails(loc.state.pid);
-      }
-    });
-    this.fetchDocumentDetails(this.props.match.params.documentPid);
+    this.props.fetchDocumentDetails(this.props.match.params.documentPid);
   }
 
-  componentWillUnmount() {
-    this.unlisten();
+  componentDidUpdate(prevProps) {
+    if (this.props.location.pathname !== prevProps.location.pathname) {
+      this.props.fetchDocumentDetails(this.props.match.params.documentPid);
+    }
   }
 
   render() {
