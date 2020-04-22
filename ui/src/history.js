4aff809b63a656c2e5ee2eb6b7051ae769e9b4a4
Nicola Tarocco <ntarocco@gmail.com>
2020-04-22 19:37:20 +0200

    ui: scroll to top on history change
    - fixes the behaviour of nothing scrolling to top when the history
      changes
    - closes #825

--- ui/src/history.js
+++ ui/src/history.js
@@ -2,6 +2,10 @@ import { createBrowserHistory } from 'history';
 
 const history = createBrowserHistory();
 
+history.listen(_ => {
+  window.scrollTo(0, 0);
+});
+
 export const goTo = (path, state = {}) => {
   history.push(path, state);
 };
