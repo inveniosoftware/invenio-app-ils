57783360ff647512f66d39aa3aac0d56abca3290
Nicola Tarocco <ntarocco@gmail.com>
2020-06-05 14:00:24 +0200

    release: v1.0.0a0 :rocket:

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,8 +1,12 @@
 ..
-    Copyright (C) 2018 CERN.
+    Copyright (C) 2018-2020 CERN.
 
     invenio-app-ils is free software; you can redistribute it and/or modify it
     under the terms of the MIT License; see LICENSE file for more details.
 
 Changes
 =======
+
+Version 1.0.0a0 (released 2020-06-05)
+
+- Initial public release.
--- invenio_app_ils/version.py
+++ invenio_app_ils/version.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2018 CERN.
+# Copyright (C) 2018-2020 CERN.
 #
 # invenio-app-ils is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -11,6 +11,4 @@ This file is imported by ``invenio_app_ils.__init__``,
 and parsed by ``setup.py``.
 """
 
-from __future__ import absolute_import, print_function
-
-__version__ = '1.0.0'
+__version__ = '1.0.0a0'
