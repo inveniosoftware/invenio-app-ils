0829818852fe5e6288667d6c5d35ac9b0863efc9
Florian Cassayre <florian.cassayre@gmail.com>
2020-09-17 10:02:20 +0200

    Holidays module: dates API * check if the library is open on a given date * find the first date where the library is open * tests

--- tests/api/ils/test_closures.py
+++ tests/api/ils/test_closures.py
@@ -9,8 +9,11 @@
 
 import json
 
+import arrow
 from flask import url_for
 
+from invenio_app_ils.closures.api import find_next_open_date
+from invenio_app_ils.proxies import current_app_ils
 from tests.helpers import user_login
 
 _LOCATION_PID = "locid-1"
@@ -45,6 +48,10 @@ def _build_location_closures_data(closed_weekdays, exceptions):
         "opening_weekdays": opening_weekdays,
         "opening_exceptions": opening_exceptions
     }
+
+
+def _date_from_string(date_string):
+    return arrow.get(date_string).date()
 
 
 def test_location_validation(client, json_headers, users, testdata):
@@ -141,3 +148,60 @@ def test_location_validation(client, json_headers, users, testdata):
     _test_update_exceptions([], [
         ["2000-01-02", "2000-01-01", True],
     ], 400)
+
+
+def test_find_next_open_date(app, db, testdata):
+    def _update_location_closures_data(closed_weekdays, exceptions):
+        Location = current_app_ils.location_record_cls
+        record = Location.get_record_by_pid(_LOCATION_PID)
+
+        record.update(
+            _build_location_closures_data(closed_weekdays, exceptions))
+        record.commit()
+        db.session.commit()
+        current_app_ils.location_indexer.index(record)
+
+        return record
+
+    def _test(start_date, expected_next_open_date):
+        next_open = find_next_open_date(
+            _LOCATION_PID, _date_from_string(start_date)
+        )
+        if expected_next_open_date:
+            assert next_open == _date_from_string(expected_next_open_date)
+        else:
+            assert next_open is None
+
+    """
+    Mon. Tue. Wed. Thu. Fri. Sat. Sun.
+     27   28   29   30   31  -01  -02
+    x03  x04  x05   06   07  -08  -09
+    x10  x11  x12  x13  x14  x15  x16
+    x17  x18  x19  x20  x21  x22  -23
+    x24   25   26  x27  x28  o29  o30
+     31   01   02   03   04  -05  -06
+    """
+
+    closed_weekdays = ["saturday", "sunday"]
+    exceptions = [
+        ["2000-01-03", "2000-01-05", False],  # Mon. - Wed.
+        ["2000-01-09", "2000-01-22", False],  # Sun. - Sat.
+        ["2000-01-24", "2000-01-24", False],  # Mon.
+        ["2000-01-27", "2000-01-28", False],  # Thu. - Fri.
+        ["2000-01-29", "2000-01-30", True],   # Sat. - Sun.
+    ]
+    _update_location_closures_data(closed_weekdays, exceptions)
+
+    _test("2000-01-01", "2000-01-06")
+    _test("2000-01-04", "2000-01-06")
+    _test("2000-01-06", "2000-01-06")
+    _test("2000-01-07", "2000-01-07")
+
+    _test("2000-01-09", "2000-01-25")
+    _test("2000-01-13", "2000-01-25")
+
+    _test("2000-01-26", "2000-01-26")
+    _test("2000-01-27", "2000-01-29")
+    _test("2000-01-30", "2000-01-30")
+
+    _test("2000-02-05", "2000-02-07")
