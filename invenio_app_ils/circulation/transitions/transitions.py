bfe973fac46dcc46ced7022740a4e30f45764706
Konstantina Stoikou <konstantinastoikou1@gmail.com>
2020-11-19 13:22:44 +0100

    Loans: fix start date bug

--- invenio_app_ils/circulation/transitions/transitions.py
+++ invenio_app_ils/circulation/transitions/transitions.py
@@ -9,18 +9,27 @@
 
 import arrow
 from invenio_circulation.transitions.transitions import (
-    ItemOnLoanToItemOnLoan, ToItemOnLoan)
+    ItemOnLoanToItemOnLoan,
+    ToItemOnLoan,
+)
 
 from invenio_app_ils.closures.api import find_next_open_date
 from invenio_app_ils.ill.api import circulation_item_location_retriever
 
 
+def _update_loan_start_date(loan):
+    """Update the end date to the next opening day."""
+    loan["start_date"] = arrow.utcnow()
+
+
 def _update_loan_end_date(loan):
     """Update the end date to the next opening day."""
-    loan["end_date"] = arrow.get(find_next_open_date(
-        circulation_item_location_retriever(loan["item_pid"]),
-        loan["end_date"].date()
-    ))
+    loan["end_date"] = arrow.get(
+        find_next_open_date(
+            circulation_item_location_retriever(loan["item_pid"]),
+            loan["end_date"].date(),
+        )
+    )
 
 
 class ILSToItemOnLoan(ToItemOnLoan):
@@ -30,6 +39,7 @@ class ILSToItemOnLoan(ToItemOnLoan):
         """Update the end date to the next opening day."""
         super().before(loan, initial_loan, **kwargs)
 
+        _update_loan_start_date(loan)
         _update_loan_end_date(loan)
 
 
