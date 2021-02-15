1adc80029e42d6b540dd797bc8ac90ff225cface
Nicola Tarocco <ntarocco@gmail.com>
2021-02-15 14:11:24 +0100

    config: add samesite cookie config

--- invenio_app_ils/config.py
+++ invenio_app_ils/config.py
@@ -285,6 +285,7 @@ SECRET_KEY = "CHANGE_ME"
 MAX_CONTENT_LENGTH = 100 * 1024 * 1024  # 100 MiB
 #: Sets cookie with the secure flag by default
 SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_SAMESITE = "Lax"
 #: Since HAProxy and Nginx route all requests no matter the host header
 #: provided, the allowed hosts variable is set to localhost. In production it
 #: should be set to the correct host and it is strongly recommended to only
