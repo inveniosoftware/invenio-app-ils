e2eeb27ba79bab4a188f20dd6fe0b4a99725cf6c
Konstantina <tinasto97@gmail.com>
2021-03-15 16:47:01 +0100

    proceedings: rename from proceeding

--- invenio_app_ils/documents/api.py
+++ invenio_app_ils/documents/api.py
@@ -39,7 +39,7 @@ class Document(IlsRecordWithRelations):
 
     DOCUMENT_TYPES = [
         "BOOK",
-        "PROCEEDING",
+        "PROCEEDINGS",
         "STANDARD",
         "SERIAL_ISSUE",
         "ARTICLE",
