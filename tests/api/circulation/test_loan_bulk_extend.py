2035bf55828dbcd688d959b7b68e795d36e3494b
nico <nicolas.rodriguezcuellar@student.hu.nl>
2021-11-23 14:23:57 +0100

    Bulk extend: test data for overdue loans
    Added test data for overdue loans in addition to expiring loans

--- tests/api/circulation/test_loan_bulk_extend.py
+++ tests/api/circulation/test_loan_bulk_extend.py
@@ -22,24 +22,37 @@ from invenio_app_ils.items.api import ITEM_PID_TYPE, Item
 from tests.api.conftest import _create_records
 from tests.helpers import user_login
 
-pid_start_value = 99
-amount_of_ongoing_loans = 5
-pid_end_value = pid_start_value + amount_of_ongoing_loans
+pid_start_value = 200
+amount_of_ongoing_loans = 3
+amount_of_overdue_loans = 3
+pid_end_value = \
+    pid_start_value + amount_of_ongoing_loans + amount_of_overdue_loans
 
 
 def create_loans(start=pid_start_value, end=pid_end_value):
     ongoing_loans = []
+    overdue_loans = []
 
     day_range_loan_extendable = \
         current_app.config["ILS_CIRCULATION_LOAN_WILL_EXPIRE_DAYS"]
 
     now = arrow.utcnow()
-    end_date_not_expired = now + timedelta(days=day_range_loan_extendable - 1)
 
-    def loan_generator(pid_value):
+    start_date_expiring = now.isoformat()
+    start_date_overdue = (now - timedelta(days=5)).isoformat()
+
+    end_date_expiring = \
+        (now + timedelta(days=day_range_loan_extendable - 1)).isoformat()
+    end_date_overdue = (now - timedelta(days=2)).isoformat()
+
+    def loan_generator(pid_value, overdue_loan=False):
+        end_date = end_date_overdue if overdue_loan else end_date_expiring
+        start_date = \
+            start_date_overdue if overdue_loan else start_date_expiring
+
         loan = {
             "document_pid": "docid-1",
-            "end_date": end_date_not_expired.isoformat(),
+            "end_date": end_date,
             "item_pid": {
                 "type": "pitmid",
                 "value": f"itemid-{pid_value}"
@@ -47,7 +60,7 @@ def create_loans(start=pid_start_value, end=pid_end_value):
             "patron_pid": "1",
             "pickup_location_pid": "locid-1",
             "pid": f"loanid-{pid_value}",
-            "start_date": now.isoformat(),
+            "start_date": start_date,
             "state": "ITEM_ON_LOAN",
             "transaction_location_pid": "locid-1",
             "transaction_user_pid": "1",
@@ -61,8 +74,10 @@ def create_loans(start=pid_start_value, end=pid_end_value):
     for number in range(start, end):
         if len(ongoing_loans) < amount_of_ongoing_loans:
             ongoing_loans.append(loan_generator(number))
+        elif len(overdue_loans) < amount_of_overdue_loans:
+            overdue_loans.append(loan_generator(number, True))
 
-    return ongoing_loans
+    return ongoing_loans + overdue_loans
 
 
 def create_items(start=pid_start_value, end=pid_end_value):
@@ -89,12 +104,15 @@ def create_items(start=pid_start_value, end=pid_end_value):
 
 
 def _prepare_data(db, repeated_run_number=0):
+    total_loans = (amount_of_ongoing_loans + amount_of_overdue_loans)
     added_value = \
-        (repeated_run_number
-         *
-         amount_of_ongoing_loans
-         +
-         repeated_run_number)
+        (
+            repeated_run_number
+            *
+            total_loans
+            +
+            repeated_run_number
+        )
 
     new_pid_start_value = pid_start_value + added_value
     new_pid_end_value = pid_end_value + added_value
@@ -124,16 +142,16 @@ def _prepare_data(db, repeated_run_number=0):
 def test_loan_extend_permissions(
     client, json_headers, users, testdata, loan_params, db
 ):
-    def _run_asserts(extended_loans, ongoing_loans, patron_pid="1"):
+    def _run_asserts(extended_loans, loans, patron_pid="1"):
 
-        assert len(extended_loans) >= len(ongoing_loans)
+        assert len(extended_loans) >= len(loans)
 
         for extended_loan in extended_loans:
-            for ongoing_loan in ongoing_loans:
-                if ongoing_loan['pid'] == extended_loan['pid']:
+            for loan in loans:
+                if loan['pid'] == extended_loan['pid']:
                     assert extended_loan['state'] == "ITEM_ON_LOAN"
                     assert extended_loan['patron_pid'] == str(patron_pid)
-                    assert extended_loan['end_date'] > ongoing_loan['end_date']
+                    assert extended_loan['end_date'] > loan['end_date']
 
     def test_patron1_can_extend_own_loans():
         patron_1_ongoing_loans = _prepare_data(db)['loans']
