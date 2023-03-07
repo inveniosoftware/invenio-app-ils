50718ca74af587a97663d253753298ee068e340a
Zacharias Zacharodimos <zacharias.zacharodimos@cern.ch>
2023-03-07 15:56:51 +0100

    search: remove es6 mappings

--- invenio_app_ils/documents/mappings/v6/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-#
-# Copyright (C) 2020 CERN.
-#
-# invenio-app-ils is free software; you can redistribute it and/or modify it
-# under the terms of the MIT License; see LICENSE file for more details.
-
-"""ILS ES v6 document mappings."""
