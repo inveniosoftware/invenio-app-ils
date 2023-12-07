6e504b68db6db8209539c85dcec00311064ee3c9
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2023-12-07 11:00:42 +0100

    tests: fix user logout

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,6 +12,11 @@ import os
 import jinja2
 import pytest
 from flask import Blueprint
+from flask_webpackext.manifest import (
+    JinjaManifest,
+    JinjaManifestEntry,
+    JinjaManifestLoader,
+)
 from invenio_access.models import ActionRoles
 from invenio_access.permissions import superuser_access
 from invenio_accounts.models import Role
@@ -19,6 +24,36 @@ from invenio_app.factory import create_app as _create_app
 from invenio_userprofiles import UserProfile
 
 from invenio_app_ils.permissions import backoffice_access_action
+
+
+#
+# Mock the webpack manifest to avoid having to compile the full assets.
+#
+class MockJinjaManifest(JinjaManifest):
+    """Mock manifest."""
+
+    def __getitem__(self, key):
+        """Get a manifest entry."""
+        return JinjaManifestEntry(key, [key])
+
+    def __getattr__(self, name):
+        """Get a manifest entry."""
+        return JinjaManifestEntry(name, [name])
+
+
+class MockManifestLoader(JinjaManifestLoader):
+    """Manifest loader creating a mocked manifest."""
+
+    def load(self, filepath):
+        """Load the manifest."""
+        return MockJinjaManifest()
+
+
+@pytest.fixture(scope="module")
+def app_config(app_config):
+    """Create test app."""
+    app_config["WEBPACKEXT_MANIFEST_LOADER"] = MockManifestLoader
+    return app_config
 
 
 @pytest.fixture()
@@ -120,3 +155,67 @@ def app_with_notifs(app):
 def create_app():
     """Create test app."""
     return _create_app
+
+
+@pytest.fixture(scope="module")
+def base_app(create_app, app_config, request, default_handler):
+    """Base application fixture (without database, search and cache).
+
+    Scope: module.
+
+    This fixture is responsible for creating the Invenio application. It
+    depends on an application factory fixture that must be defined by the user.
+
+    .. code-block:: python
+
+        # confest.py
+        import pytest
+
+        @pytest.fixture(scope='module)
+        def create_app():
+            from invenio_app.factory import create_api
+            return create_api
+
+    It is possible to overide the application factory for a specific test
+    module, either by defining a fixture like above example, or simply setting
+    the ``create_app`` property on the module:
+
+    .. code-block:: python
+
+        # test_something.py
+
+        from invenio_app.factory import create_api
+        create_app = create_api
+
+        def test_acase(base_app):
+            # ...
+    """
+    # Use create_app from the module if defined, otherwise use default
+    # create_app fixture.
+    create_app = getattr(request.module, "create_app", create_app)
+    app_ = create_app(**app_config)
+
+    def delete_user_from_g(exception):
+        """Delete user from `flask.g` when the request is tearing down.
+
+        Flask-login==0.6.2 changed the way the user is saved i.e uses `flask.g`.
+        Flask.g is pointing to the application context which is initialized per
+        request. That said, `pytest-flask` is pushing an application context on each
+        test initialization that causes problems as subsequent requests during a test
+        are detecting the active application request and not popping it when the
+        sub-request is tearing down. That causes the logged in user to remain cached
+        for the whole duration of the test. To fix this, we add an explicit teardown
+        handler that will pop out the logged in user in each request and it will force
+        the user to be loaded each time.
+        """
+        from flask import g
+
+        if "_login_user" in g:
+            del g._login_user
+
+    app_.teardown_request(delete_user_from_g)
+
+    # See documentation for default_handler
+    if default_handler:
+        app_.logger.addHandler(default_handler)
+    yield app_
