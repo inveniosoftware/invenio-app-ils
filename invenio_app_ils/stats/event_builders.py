ea2c35aa73594a6f2d4cf85732d0a8456333a827
jakob miesner <jakob.miesner@gmail.com>
2025-10-10 10:02:37 +0200

    stats: add stats to track ILSRecord insertions, deletions and updates

---
+++ invenio_app_ils/stats/event_builders.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2025 CERN.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details
+
+"""ILS stats event builders."""
+
+import datetime
+
+from flask import g
+from flask_login import current_user
+
+from invenio_app_ils.permissions import backoffice_permission
+from invenio_app_ils.records.api import IlsRecord
+
+
+def ils_record_changed_event_builder(
+    event, sender_app, method=None, record=None, **kwargs
+):
+    """Build an event for ILS record changes."""
+
+    if not method:
+        raise ValueError("Method must be provided to the event builder.")
+    if not record:
+        raise ValueError("Record must be provided to the event builder.")
+    if not isinstance(record, IlsRecord):
+        return None
+
+    # We have to get the pid_type directly through the private accessor,
+    # as the pid might not be registered directly after the record creation.
+    pid_type = record._pid_type
+
+    event_data = {
+        "timestamp": datetime.datetime.now(datetime.timezone.utc)
+        .replace(tzinfo=None)
+        .isoformat(),
+        "pid_type": pid_type,
+        "method": method,
+    }
+
+    event.update(event_data)
+    return event
+
+
+def add_librarian_user_id_to_event(event, sender_app, **kwargs):
+    """Add the user_id of the librarian performing the action to the event."""
+
+    user_id = None
+    if (
+        current_user
+        and current_user.is_authenticated
+        and backoffice_permission().allows(g.identity)
+    ):
+        user_id = current_user.get_id()
+
+    event.update({"user_id": user_id})
+
+    return event
+
+
+def add_record_pid_to_event(event, sender_app, record=None, **kwargs):
+    """Add the record pid to the event."""
+    if not record:
+        raise ValueError("Record must be provided to the event builder.")
+
+    # We have to get the pid directly from the record,
+    # as the pid might not yet be registered (e.g. after record creation).
+    event.update({"pid_value": record.get("pid")})
+
+    return event
---
+++ invenio_app_ils/stats/processors.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2025 CERN.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details
+
+"""ILS stats preprocessors."""
+
+
+def add_record_change_ids(doc):
+    """Add unique_id and aggregation_id to the doc."""
+
+    # We use this field to group by during aggregation.
+    # e.g. the count of created eitems by a user with id 7 is tracked under eitmid__insert__None.
+    doc["aggregation_id"] = f"{doc['pid_type']}__{doc['method']}__{doc['user_id']}"
+
+    # unique_id identifies each individual event and is used by invenio-stats.
+    # It automatically deduplicates events from the same second that have the same unique_id.
+    # Including the pid_value ensures distinctness between events,
+    # even when multiple records are updated within the same second.
+    # e.g. during the importer in cds-ils where many eitems are created in bulk.
+    doc["unique_id"] = (
+        f"{doc['pid_value']}__{doc['pid_type']}__{doc['method']}__{doc['user_id']}"
+    )
+
+    return doc
