9fdf5821a543b6371afdd8a4853970f1c037282c
jakob miesner <jakob.miesner@gmail.com>
2026-01-05 21:16:42 +0100

    stats: add feature flag for extending record indices with stats

--- tests/api/ils/stats/conftest.py
+++ tests/api/ils/stats/conftest.py
@@ -17,3 +17,12 @@ def empty_event_queues():
         queue = current_stats.events[event].queue
         queue.queue.declare()
         queue.consume()
+
+@pytest.fixture()
+def with_stats_index_extensions(app):
+    """Enable indices to be extended with stats data."""
+    app.config["ILS_EXTEND_INDICES_WITH_STATS_ENABLED"] = True
+    yield
+    app.config["ILS_EXTEND_INDICES_WITH_STATS_ENABLED"] = False
+
+
